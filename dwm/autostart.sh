9921477e0b6fe65abdcb32f615cf1538eaa560ff
TrudeEH <trudeeh@gmail.com>
2021-11-28 16:51:22 +0000

    Added wallpaper support

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,1 +1,2 @@
-
+killall compton ; compton --backend glx --unredir-if-possible --vsync opengl-swc &
+nitrogen --restore &
--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+sudo apt install -y compton nitrogen
+
+
+install_nitrogen () {
+    # Dependencies:
+    # + nitrogen (APT)  (Wallpaper)
+    # + nitrogen (REPO) (nitrogen config file)
+    # + bg.png   (REPO) (Default wallpaper)
+
+    sudo mkdir -p /usr/share/wallpapers/
+    sudo cp bg.png /usr/share/wallpapers/
+    cp -r nitrogen ~/.config/
+}
+
+
 cd dwm/
 mkdir -p ~/.dwm
 cp -f autostart.sh ~/.dwm
@@ -13,3 +28,5 @@ cd ..
 cd st/
 sudo make clean install
 cd ..
+
+install_nitrogen
