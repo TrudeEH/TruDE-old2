e0605b3adfefada49ad497277158807f9427084e
TrudeEH <ehtrude@gmail.com>
2021-11-30 11:10:51 +0000

    Dwm and Dmenu colors changed to match st's

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -9,11 +9,11 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#d8dee9", "#2e3440" },
+	[SchemeSel] = { "#d8dee9", "#5e81ac" },
+	[SchemeSelHighlight] = { "#ebcb8b", "#5e81ac" },
+	[SchemeNormHighlight] = { "#ebcb8b", "#2e3440" },
+	[SchemeOut] = { "#2e3440", "#5e81ac" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
