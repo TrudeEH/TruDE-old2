b6dc77cc11d3e186c7dfa8457eddd84628c171dd
TrudeEH <ehtrude@gmail.com>
2022-03-20 17:13:54 +0000

    Updated dmenu to match the system bar.

--- dmenu/dmenu.c
+++ dmenu/dmenu.c
@@ -37,6 +37,12 @@ struct item {
 static char text[BUFSIZ] = "";
 static char *embed;
 static int bh, mw, mh;
+
+/* bar padding */
+static int dmx = 10; /* put dmenu at this x offset */
+static int dmy = 10; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static unsigned int dmw = 2372; /* make dmenu this wide */
+
 static int inputw = 0, promptw;
 static int lrpad; /* sum of left and right padding */
 static size_t cursor;
@@ -794,9 +800,9 @@ setup(void)
 				if (INTERSECT(x, y, 1, 1, info[i]))
 					break;
 
-		x = info[i].x_org;
-		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
-		mw = info[i].width;
+		x = info[i].x_org + dmx;
+		y = info[i].y_org + (topbar ? dmy : info[i].height - mh - dmy);
+		mw = (dmw>0 ? dmw : info[i].width);
 		XFree(info);
 	} else
 #endif
@@ -804,9 +810,9 @@ setup(void)
 		if (!XGetWindowAttributes(dpy, parentwin, &wa))
 			die("could not get embedding window attributes: 0x%lx",
 			    parentwin);
-		x = 0;
-		y = topbar ? 0 : wa.height - mh;
-		mw = wa.width;
+		x = dmx;
+		y = topbar ? dmy : wa.height - mh - dmy;
+		mw = (dmw>0 ? dmw : wa.width);
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
@@ -848,7 +854,8 @@ static void
 usage(void)
 {
 	fputs("usage: dmenu [-bfiv] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
-	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
+	    "             [-x xoffset] [-y yoffset] [-z width]\n"
+	    "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
 }
 
@@ -875,6 +882,12 @@ main(int argc, char *argv[])
 		/* these options take one argument */
 		else if (!strcmp(argv[i], "-l"))   /* number of lines in vertical list */
 			lines = atoi(argv[++i]);
+                else if (!strcmp(argv[i], "-x"))   /* window x offset */
+                        dmx = atoi(argv[++i]);
+                else if (!strcmp(argv[i], "-y"))   /* window y offset (from bottom up if -b) */
+                        dmy = atoi(argv[++i]);
+                else if (!strcmp(argv[i], "-z"))   /* make dmenu this wide */
+                        dmw = atoi(argv[++i]);
 		else if (!strcmp(argv[i], "-h")) { /* minimum height of one menu line */
 			lineheight = atoi(argv[++i]);
 			lineheight = MAX(lineheight, min_lineheight);
