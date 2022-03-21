9ca2a9dd454afe736e87dc7dae521e6db1bc738b
TrudeEH <ehtrude@gmail.com>
2022-03-21 14:39:40 +0000

    Minor bug fixes

--- dwm/config.h
+++ dwm/config.h
@@ -9,7 +9,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[] = {
+	"monospace:size=10",
+	"FontAwesome:size=12",
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#3b4252";
--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -77,7 +77,7 @@ darkblue = #7292b2
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ run_command, " %s ",  "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ cpu_perc, " %s%% ",        NULL },
+	{ cpu_perc, "  %s%% ",        NULL },
 	{ ram_perc, " %s%% ",        NULL },
 	{ disk_free, " %s ",     "/" },
 	{ battery_perc, " %s%%", "BATT"},
