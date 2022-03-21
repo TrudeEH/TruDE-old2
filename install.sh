890dba9234b960c0b07ce70e124c7b61e65d3033
TrudeEH <ehtrude@gmail.com>
2022-03-21 21:08:38 +0000

    Added fedora utils to the installer script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-fedora () {
+fedora() {
 	# Install Script Dependencies
 	sudo dnf install -y dialog
 	dialog --msgbox  "Welcome to TruDE installer.\nPress OK to proceed." 10 50
@@ -16,9 +16,25 @@ fedora () {
 	clear
 }
 
-install_nitrogen () {
+fedora_config() {
+	# Enable rpm fusion
+	dialog   --mixedgauge   "Installing rpm fusion..." 10 50 0
+	sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &> /dev/null
+
+	# Install Preload
+	dialog   --mixedgauge   "Installing preload..." 10 50 65
+	sudo dnf copr enable elxreno/preload -y &> /dev/null && sudo dnf install preload -y &> /dev/null
+
+	# Enable flathub
+	dialog   --mixedgauge   "Installing flathub..." 10 50 100
+	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &> /dev/null
+        sleep 0.5
+        clear
+}
+
+install_nitrogen() {
     # Dependencies:
-    # + nitrogen (APT)  (Wallpaper)
+    # + nitrogen (PKG)  (Wallpaper)
     # + nitrogen (REPO) (nitrogen config file)
     # + bg.png   (REPO) (Default wallpaper)
 
@@ -27,25 +43,25 @@ install_nitrogen () {
     cp -r nitrogen ~/.config/
 }
 
-compile () {
-	dialog   --mixedgauge   "Compiling TruDE..." 10 50 0 
+compile() {
+	dialog   --mixedgauge   "Compiling TruDE..." 10 50 0
 	cd dwm/ &> /dev/null
 	mkdir -p ~/.dwm &> /dev/null
 	cp -f autostart.sh ~/.dwm &> /dev/null
 	sudo make clean install &> /dev/null
 	cd ..
-	
-	dialog   --mixedgauge   "Compiling TruDE..." 10 50 25 
+
+	dialog   --mixedgauge   "Compiling TruDE..." 10 50 25
 	cd dmenu/ &> /dev/null
 	sudo make clean install &> /dev/null
 	cd ..
-	
-	dialog   --mixedgauge   "Compiling TruDE..." 10 50 50 
+
+	dialog   --mixedgauge   "Compiling TruDE..." 10 50 50
 	cd st/ &> /dev/null
 	sudo make clean install &> /dev/null
 	cd ..
 
-	dialog   --mixedgauge   "Compiling TruDE..." 10 50 75 
+	dialog   --mixedgauge   "Compiling TruDE..." 10 50 75
 	cd slstatus/ &> /dev/null
 	sudo make clean install &> /dev/null
 	cd ..
@@ -55,9 +71,13 @@ compile () {
 }
 
 fedora
+fedora_config
 compile
 install_nitrogen
 rm -rf */*.o
+
+dialog --msgbox  "Done." 10 50
+clear
 
 # DE dependencies
 # Arch Install
